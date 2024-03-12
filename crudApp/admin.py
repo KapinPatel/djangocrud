dcf21b6720557e04f8037eb0507a934c8479a977
Kapin Patel <103712227+KapinPatel6261221355@users.noreply.github.com>
2024-03-12 12:58:09 +0530

    API and model full updated

--- crudApp/admin.py
+++ crudApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from crudApp.models import Company, Employee
 # Register your models here.
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=('name','location')
+    search_fields=('name',)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('name','phone','company')
+    list_filter=('company',)
+
+admin.site.register(Company, CompanyAdmin )
+admin.site.register(Employee,EmployeeAdmin)
